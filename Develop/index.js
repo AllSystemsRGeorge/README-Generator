64a789f2ea6155e8c63e84e5f921d4e5c70f4d33
AllSystemsRGeorge <96609501+AllSystemsRGeorge@users.noreply.github.com>
2022-06-24 20:34:20 -0700

    Finishing Index

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,47 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message:'What is your name?',
+    },
+    {
+        type: 'input',
+        name: 'quest',
+        message: 'What is your quest?',
+    },
+    {
+        type: 'list',
+        name: 'color',
+        message: 'What is your favorite color?',
+        choices: ['Red', 'Yellow', 'Blue', "I don't know!"],
+    },
+    {
+        type: 'list',
+        name: 'swallow',
+        message: 'What is the average flight speed of a swallow?',
+        choices: ['African?', 'European?'],
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquiere.prompt(questions).then((inquirerResponses)=>{
+        console.log('Generate README...');
+        writeToFile('README.md', generateMarkdown({...inquirerResponses}));
+    });
+}
 
 // Function call to initialize app
 init();
